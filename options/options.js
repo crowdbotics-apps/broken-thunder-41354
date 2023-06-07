69be5ef2c40d6a108d6484fb6a87055467b68b9e
Crowdbotics <team@crowdbotics.com>
2023-06-07 23:14:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "broken-thunder-41354",
+  url: "https://broken-thunder-41354.botics.co",
+  api: "https://broken-thunder-41354.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
